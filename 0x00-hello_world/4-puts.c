32fe6dbeb95a713022e411d6e06ee02040022229
InemGIBSON <inemgibs@gmail.com>
2022-09-12 21:30:55 -0700

    4 Puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+include <stdio.h>
+
+/**
+ *
+ * * main - print the stiring in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program will print "Programming is like building a multilingual puzzle
+ *
+ * * Return:0
+ *
+ * */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}

a52a32b710ff09494f010ec1ecb0c0b24bd290f2
InemGIBSON <inemgibs@gmail.com>
2022-09-16 19:37:45 -0700

    ISUPPER

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for prototypes
+*void prototypes (void)
+*int prototypes(int)
+*/
+
+void print_numbers(void);
+void print_most_numbers(void);
+void print_more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+void _putchar(char);
+void _isupper(int);
+void _isdigit(int);
+void mul(int, int);
+
+#endif /* MAIN.H */

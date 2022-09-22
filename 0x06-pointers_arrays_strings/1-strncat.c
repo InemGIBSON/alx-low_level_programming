67fdf2bed9fe9a4cfb423a65d38cd16ed4d00829
InemGIBSON <inemgibs@gmail.com>
2022-09-21 18:51:11 -0700

    Task 1 Commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat -> function to append some characters
+ * @dest:first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest)
+}
